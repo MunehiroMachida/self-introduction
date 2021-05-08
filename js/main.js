05ebfe394c9497e0b0c59a80084e6c36eacba609
MUnehiroMachida <life1231@outlook.jp>
2021-05-08 16:45:55 +0900

    お問い合わせ追加

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#slider").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        dots: true,
+        fade: true
+    });
+});
+
+
+// ページ遷移
+$('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+});
